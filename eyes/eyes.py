8bcadf139f1ff864d5ce2e9edc182e1ddc723abd
Chovin <ccarlson@docomopacific.com>
2017-04-20 02:36:59 +1000

    [wip] eyes half converted to cog
    sorry, not very github-flowy
    
    still not in working condition.
    lots of settings though:
    [p]eyes channel  # log a channel
    [p]eyes server  # act like toggling all channels
    [p]eyes list  # list logged channels (should we have something about servers? maybe in PM)
    [p]eyes timezone  # tries to get current tz by default, but lets you choose
    [p]eyes bell  # settings for ANSI bell
    [p]eyes bell toggle  # toggle whole server or individual channels (keep an eye out for this later)
    [p]eyes bell user  # toggles watching for a username in server
    [p]eyes bell me  # toggles watching bot owner everywhere
    [p]eyes bell regex  # toggles specific regex. (add global regex later)

--- eyes/eyes.py
+++ eyes/eyes.py
@@ -1,8 +1,21 @@
+import discord
+from discord.ext import commands
+from cogs.utils.dataIO import dataIO
+from cogs.utils import checks
+import red
+from __main__ import send_cmd_help
+import asyncio
 import re
+import time
 import datetime as dt
-TZ_OFFSET = dt.timedelta(hours=10)
+from copy import deepcopy
+from random import randint
+from random import choice as randchoice
 
-PAT_ME = re.compile(r"(@?{}\b|\bird?\b|\bdumbs?\b|\birdumbs?\b)".format(author.display_name))
+SETTINGS_PATH    = "data/eyes/settings.json"
+STRF             = "%I:%M%p"
+EMPTY_REGEX_SETS = {"USERS": [], "RAW": []}
+
 
 class C:
     HEADER    = '\033[95m'
@@ -30,32 +43,286 @@ class C:
     def undeline(c):
         return C.UNDERLINE + c + C.ENDC
 
-def highlight_me(c):
-    resume = C.ENDC
-    if c.startswith(C.WARNING):
-        resume = C.WARNING
-    if c.startswith(C.FAIL):
-        resume = C.FAIL
-    return re.sub(PAT_ME, C.ENDC + C.BELL + C.OKBLUE + r'\g<0>' + resume, c)
 
-def pad(c):
-    return ' {} '.format(c)
+# REGEX:
+# "{@100406911567949824.display_name}".format(**{'@'+m.id: m for m in server.members})
+# owner + | + '|'.join(global + sv_users + sv_regex)
 
-def max_channel_length(m):
-    return max([len(c.name) for c in m.server.channels 
-                if c.type == m.channel.type])
 
-async def shhh(m):
-    if m.server.id != '133049272517001216':
-        return
+class Eyes:
+    """👀"""
 
-    c = highlight_me(pad(m.clean_content))
-    ms = dt.datetime.now().strftime('%H:%M%p')
-    mcl = max_channel_length(m)
-    ms += ' #{:<{}} @{}:{}'.format(m.channel.name, mcl, m.author, c)
-    print(ms)
+    def __init__(self, bot):
+        self.bot = bot
+        self.settings = dataIO.load_json(SETTINGS_PATH)
+        self.bot.loop.create_task(self.update_me_help())
 
-async def shhhdel(m):
+    async def update_me_help(self):
+        await self.bot.wait_until_ready()
+        owner = next(m for m in self.bot.get_all_members()
+                     if m.id == self.bot.settings.owner)
+        self.eyes_bell_me.help = self.eyes_bell_me.help.format(owner.name)
+
+    """
+    def can_bell(self, channel_or_server):
+        cs = channel_or_server
+        bells = self.settings['BELLS']
+        return self.bell_id(channel_or_server) in bells"""
+
+    def bell_id(self, channel_or_server):
+        cs = channel_or_server
+        if isinstance(cs, discord.Server):
+            return 'S' + cs.id
+        return cs.id
+
+    def save(self):
+        return dataIO.save_json(SETTINGS_PATH, self.settings)
+
+    def highlight_me(self, content, server):
+        resume = C.ENDC
+        if content.startswith(C.WARNING):
+            resume = C.WARNING
+        if content.startswith(C.FAIL):
+            resume = C.FAIL
+        return re.sub(self.settings['BELL_PATTERN'],
+                      C.ENDC + C.BELL + C.OKBLUE + r'\g<0>' + resume,
+                      content)
+
+    async def respond(self, yes):
+        if yes:
+            return await self.bot.say(":eyes:")
+        m = await self.bot.say(":hear_no_evil:")
+        await asyncio.sleep(.5)
+        await self.bot.edit_message(m, ":see_no_evil:")
+
+    @checks.is_owner()
+    @commands.group(pass_context=True)
+    async def eyes(self, ctx):
+        """settings for your 👀"""
+        if ctx.invoked_subcommand is None:
+            await send_cmd_help(ctx)
+
+    @eyes.command(pass_context=True, name="server", no_pm=True)
+    async def eyes_server(self, ctx):
+        """you can't hide 👀"""
+        server = ctx.message.server
+        serv_sets = self.settings['SERVERS']
+        chan_sets = self.settings['CHANNELS']
+        try:  # lol
+            serv_sets.remove(server.id)
+        except ValueError:
+            serv_sets.append(server.id)
+        else:  # more natural
+            chan_sets = [cid for cid in chan_sets
+                         if not server.get_channel(cid)]
+        self.save()
+        await self.respond(server.id in serv_sets)
+
+    @eyes.command(pass_context=True, name="channel", no_pm=True)
+    async def eyes_channel(self, ctx, channel: discord.Channel=None):
+        """channel 👀
+        defaults to current"""
+        channel = channel or ctx.message.channel
+        if channel.type is not discord.ChannelType.text:
+            ugh = randchoice(":neutral_face: :confused: :expressionless: "
+                             ":unamused: :rolling_eyes: :thinking:".split())
+            await self.bot.say(ugh)
+            return
+        chan_sets = self.settings['CHANNELS']
+        try:
+            chan_sets.remove(channel.id)
+        except ValueError:
+            chan_sets.append(channel.id)
+        self.save()
+        await self.respond(channel.id in chan_sets)
+
+    @eyes.command(pass_context=True, name="list", no_pm=True)
+    async def eyes_list(self, ctx):
+        """list 👀s in server"""
+        server        = ctx.message.server
+        me            = server.me
+        text_channels = [c for c in server.channels
+                         if c.type is discord.ChannelType.text]
+
+        serv_sets = self.settings['SERVERS']
+        chan_sets = self.settings['CHANNELS']
+
+        if server.id in serv_sets:
+            chans = [c for c in text_channels
+                     if c.permissions_for(me).read_messages]
+        else:
+            chans = filter(None,
+                           (server.get_channel(cid) for cid in chan_sets))
+
+        msg = "I have :eyes:s in:\n"
+        if chans > len(text_channels) / 2:
+            msg = "I don't have :eyes:s in:\n"
+            chans = set(text_channels) - chans
+
+        await self.bot.say(msg + '\n'.join(['\t' + c.name for c in chans]))
+
+    @eyes.command(pass_context=True, name="timezone")
+    async def eyes_timezone(self, ctx, utc_offset=None):
+        """UTC+? 👀
+        defaults to system's"""
+        discord_time = ctx.message.timestamp
+        utc_offset = utc_offset or local_timezone()
+        self.settings['TZ_OFFSET'] = utc_offset
+        self.save()
+        now = dt.datetime.utcnow() - dt.timedelta(hours=utc_offset)
+        msg = 'You set your :eyes: to UTC+{}. '.format(utc_offset)
+        msg += ('That means it should be {} for you right now'
+                .format(now.strftime(STRF)))
+        await self.bot.say(msg)
+
+    @eyes.group(pass_context=True, name="bell", aliases=["alert"])
+    async def eyes_bell(self, ctx):
+        """🔔 and regex settings"""
+        if ctx.invoked_subcommand is None or \
+                isinstance(ctx.invoked_subcommand, commands.Group):
+            await send_cmd_help(ctx)
+            return
+
+    @eyes_bell.command(pass_context=True, name="toggle", no_pm=True)
+    async def eyes_bell_toggle(self, ctx, channel: discord.Channel=None):
+        """👀 🔔/🔕
+        defaults to whole server"""
+        server = ctx.message.server
+        cs = channel or server
+        bid = self.bell_id(cs)
+        bell_sets = self.settings['BELLS']
+        try:
+            bell_sets.remove(bid)
+        except ValueError:
+            bell_sets.append(bid)
+        self.save()
+
+        if bid in bell_sets:
+            return await self.bot.say('{} :bell:'.format(cs))
+        await self.bot.say('{} :no_bell:'.format(cs))
+
+    @eyes_bell.command(pass_context=True, name="user", no_pm=True)
+    async def eyes_bell_user(self, ctx, user: discord.Member=None):
+        """👀 🔔/🔕 👤
+        leave blank to list users
+        use "[p]eyes bell me" for cross-server alerts for yourself"""
+        server = ctx.message.server
+        empty = deepcopy(EMPTY_REGEX_SETS)
+        user_pats = self.settings["BELL_PATTERNS"].setdefault(server.id, empty)
+        user_pats = user_pats["USERS"]
+        if user is None:  # list users
+            if not user_pats:
+                await self.bot.say('nobody :no_bell:')
+            else:
+                msg = ':bell: on: '
+                msg += ', '.join(user_pats)
+                await self.bot.say(msg)
+            return
+
+        try:
+            user_pats.remove(user.id)
+        except ValueError:
+            user_pats.append(user.id)
+        self.save()
+        if user.id in user_pats:
+            return await self.bot.say(":bell: on {} `@?\{@{}.display_name\}`\n"
+                                      "see `{}help eyes ball regex` for an explanation"
+                                      .format(user, user.id, ctx.prefix))
+        await self.bot.say(":no_bell: on {}".format(user))
+
+    @eyes_bell.command(pass_context=True, name="me")
+    async def eyes_bell_me(self, ctx):
+        """👀 🔔/🔕 {} cross-server"""  # owner added later
+        author = ctx.message.author
+        bell_pat = self.settings["BELL_PATTERNS"]
+        bell_pat["OWNER"] = not bell_pat["OWNER"]
+        self.save()
+        if bell_pat["OWNER"]:
+            return await self.bot.say(":bell: everywhere on `@?\{@{}.display_name\}`\n"
+                                      "see `{}help eyes ball regex` for an explanation"
+                                      .format(author.id, ctx.prefix))
+        await self.bot.say(":no_bell:")
+
+    @eyes_bell.command(pass_context=True, name="regex", no_pm=True)  # pm when global
+    async def eyes_bell_regex(self, ctx, pattern: str=None):
+        """👀 🔔/🔕 toggle regex directly
+        leave blank to list regexes of the current environment
+
+        """  # use in pm to toggle global regexes
+        server = ctx.message.server
+        channel = ctx.message.channel
+        author = ctx.message.author
+        empty = deepcopy(EMPTY_REGEX_SETS)
+        re_pats = self.settings["BELL_PATTERNS"].setdefault(server.id, empty)
+        re_pats
+
+    def get_content(m):
+        if not m.clean_content:
+            embed = [e for e in m.embeds if e['type'] == 'rich']  # ?
+            return pad('[EMBED] TITLE: ' + embed['title'])
+        return pad(content)
+
+    async def on_message(self, m):
+        if m.server.id != '133049272517001216':
+            return
+
+        c = self.highlight_me(pad(m.clean_content))
+        ms = dt.datetime.now().strftime('%H:%M%p')
+        mcl = max_channel_length(m)
+        ms += ' #{:<{}} @{}:{}'.format(m.channel.name, mcl, m.author, c)
+        print(ms)
+
+
+def local_timezone():
+    return -(time.timezone
+             if (time.localtime().tm_isdst == 0)
+             else time.altzone) / 60 / 60
+
+
+def pad(content):
+    return ' {} '.format(content)
+
+
+def max_channel_length(msg):
+    return max([len(c.name) for c in msg.server.channels
+                if c.type == msg.channel.type])
+
+
+def check_folders():
+    paths = ("data/eyes", )
+    for path in paths:
+      if not os.path.exists(path):
+          print("Creating {} folder...".format(path))
+          os.makedirs(path)
+
+
+def check_files(bot):
+    default = {'SERVERS': [], 'CHANNELS': [], 'BELLS': [],
+               'BELL_PATTERNS': {"OWNER": True, "GLOBALS": []},
+               'TZ_OFFSET': local_timezone()}
+
+    if not dataIO.is_valid_json(SETTINGS_PATH):
+        print("Creating default eyes settings.json...")
+        dataIO.save_json(SETTINGS_PATH, default)
+    else:  # consistency check
+        current = dataIO.load_json(SETTINGS_PATH)
+        if current.keys() != default.keys():
+            for key in default.keys():
+                if key not in current.keys():
+                    current[key] = default[key]
+                    print(
+                        "Adding " + str(key) + " field to eyes settings.json")
+            dataIO.save_json(SETTINGS_PATH, current)
+
+
+def setup(bot: red.Bot):
+    check_folders()
+    check_files(bot)
+    n = Eyes(bot)
+    bot.add_cog(n)
+
+
+async def on_message_delete(m):
     if m.server.id != '133049272517001216':
         return
 
@@ -66,7 +333,8 @@ async def shhhdel(m):
     ms += ' #{:<{}} DELETED: {} @{}:{}'.format(m.channel.name, mcl, ot, m.author, c)
     print(C.red(ms))
 
-async def shhhedit(o,m):
+
+async def on_message_edit(o,m):
     if m.server.id != '133049272517001216':
         return
 
@@ -82,13 +350,14 @@ async def shhhedit(o,m):
     oss = (o.timestamp + TZ_OFFSET).strftime('%H:%M%p')
     mcl = max_channel_length(m)
     oss += ' #{:<{}} OLD: @{}:{}'.format(o.channel.name, mcl, o.author, oc)
-    ms += ' #{:<{}} {} @{}:{}'.format(m.channel.name, mcl, 
+    ms += ' #{:<{}} {} @{}:{}'.format(m.channel.name, mcl,
                                        C.HEADER+"NEW:"+C.WARNING, m.author, mc)
     print(C.yellow(oss))
     print(C.yellow(ms))
 
-def shhhpin(m):
-    pinned = " PINNED: " if m.pinned else " {}PINNED: ".format(C.BOLD+"UN"+C.ENDC+C.OKGREEN)
+
+def on_message_pin(m):
+    pinned = "PINNED: " if m.pinned else "{}PINNED: ".format(C.BOLD+"UN"+C.ENDC+C.OKGREEN)
 
     c = highlight_me(C.green(pad(m.clean_content)))
     ms = dt.datetime.now().strftime('%H:%M%p')
@@ -101,7 +370,9 @@ bot.add_listener(shhh, 'on_message')
 bot.add_listener(shhhdel, 'on_message_delete')
 bot.add_listener(shhhedit, 'on_message_edit')
 
-ugh = bot 
+ugh = bot
+
+
 @ugh.command()
 async def boop():
     m = await bot.say(C.BELL)
